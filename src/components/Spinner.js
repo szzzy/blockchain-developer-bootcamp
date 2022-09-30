2191702924b38d37c2d685c787313716b1a7e1cb
szzzy <1925861393@qq.com>
2022-09-30 04:13:16 -0400

    finish a trade view for cryptocurrency exchange

---
+++ src/components/Spinner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+//for better load
+export default function ({ type }) {
+	if(type === 'table') {
+		return(<tbody className="spinner-border text-light text-center"></tbody>)
+	} else {
+		return(<div className="spinner-border text-light text-center"></div>)
+	}
+}
--- src/components/Trades.js
+++ src/components/Trades.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import Spinner from './Spinner'
 import {
 	filledOrdersLoadedSelector,
 	filledOrdersSelector
 } from '../store/selectors'
 
 const showFilledOrders = (filledOrders) => {
+	//<tr className={`order-${order.id}`} key={order.id}> //it must have a key to show that each table row is unique
 	return(
 		<tbody>
 			{ filledOrders.map((order) => {
 				return(
-					<tr>
-						<td>Foo</td>
-						<td>Bar</td>
-						<td>BB</td>
+					<tr className={`order-${order.id}`} key={order.id}>
+						<td className="text-muted">{order.formattedTimestamp}</td>
+						<td>{order.tokenAmount}</td>
+						<td className={`text-${order.tokenPriceClass}`}>{order.tokenPrice}</td>
 					</tr>
 				)
 			}) }
@@ -24,25 +26,29 @@ const showFilledOrders = (filledOrders) => {
 class Trades extends Component {
 	render() {
 		return (
-			<div className="card bg-dark text-white">
-				<div className="card-header">
-					card title6
-				</div>
-				<div className="card-body">
+			<div className="vertical">
+				<div className="card bg-dark text-white">
 
-					<table className="table table-dark table-sm small">
-						<thead>
-							<tr>
-								<th>Time</th>
-								<th>DAPP</th>
-								<th>DAPP/ETH</th>
-								{ this.props.filledOrdersLoaded ? showFilledOrders(this.props.filledOrders) : <tbody></tbody> }
-							</tr>
-						</thead>
-					</table>
+					<div className="card-header">
+						Trades
+					</div>
+
+					<div className="card-body">
+						<table className="table table-dark table-sm small">
+							<thead>
+								<tr>
+									<th>Time</th>
+									<th>DAPP</th>
+									<th>DAPP/ETH</th>
+								</tr>
+							</thead>
+							{ this.props.filledOrdersLoaded ? showFilledOrders(this.props.filledOrders) : <Spinner type="table" /> }
+						</table>
+
+					</div>
 
 				</div>
-			</div>
+			</div>	
 		)
 	}
 }
