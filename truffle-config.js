5428a6383b434a494f762c785c9f528bba8607c2
szzzy <1925861393@qq.com>
2022-11-20 03:32:58 -0500

    deploy ui finally

--- truffle-config.js
+++ truffle-config.js
@@ -2,7 +2,8 @@ require("@babel/register");
 require("@babel/polyfill");
 require('dotenv').config();
 //const HDWalletProvider = require('truffle-hdwallet-provider');
-const HDWalletProvider = require("@truffle/hdwallet-provider");
+//const HDWalletProvider = require("@truffle/hdwallet-provider");
+const HDWalletProvider = require('truffle-hdwallet-provider')
 const privateKeys = process.env.PRIVATE_KEYS || ""
 const infuraApiKey = process.env.INFURA_API_KEY || ""
 
@@ -15,12 +16,19 @@ module.exports = {
     },
     goerli: {
       //networkCheckTimeout: 60000, // 超过 1min 则提示超时
+      // provider: function() {
+      //   return new HDWalletProvider({
+      //     //Private Key
+      //     privateKeys: privateKeys, //array of account private keys
+      //     providerOrUrl: `https://goerli.infura.io/v3/${infuraApiKey}`  //Url to Etherum Node
+      //   })
+      // },
+      networkCheckTimeout: 60000,
       provider: function() {
-        return new HDWalletProvider({
-          //Private Key
-          privateKeys: privateKeys, //array of account private keys
-          providerOrUrl: `https://goerli.infura.io/v3/${infuraApiKey}`  //Url to Etherum Node
-        })
+        return new HDWalletProvider(
+          privateKeys, // Array of account private keys
+          `https://goerli.infura.io/v3/${infuraApiKey}` // Url to an Ethereum Node
+        )
       },
       gas: 5000000,
       gasPrice: 25000000000,
